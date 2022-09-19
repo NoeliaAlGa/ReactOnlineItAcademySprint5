ac2006af3f09c2884538fdab25649806092cd3b1
NoeliaAlvarez <noelia.ag1990@gmail.com>
2022-09-19 12:03:51 +0200

    N2 Exercici 4 API elTemps

--- first.js
+++ first.js
@@ -5,8 +5,10 @@ var request = {
         "Accept": "application/json"
     }
 };
+var API_URL_ELTIEMPO = 'https://api.openweathermap.org/data/2.5/weather?lat=41.56667&lon=2.01667&appid=85cfa2c7d7ecfaba7ec9e19c3e057d04';
 var actualJoke = "";
 var reportAcudits = [];
+var tempsActualTerrassa = "";
 function nextJoke() {
     var jokeText = document.getElementById("apiText");
     fetch(API_URL, request)
@@ -27,4 +29,15 @@ function jokeNote(note) {
     };
     reportAcudits.push(jokesNotes);
     console.log(reportAcudits);
+}
+function elTiempo() {
+    var textElTempsActual = document.getElementById('elTemps');
+    var elTempsTerrassa = [];
+    fetch(API_URL_ELTIEMPO)
+        .then(function (response) { return response.json(); })
+        .then(function (tempsTerrassa) {
+        elTempsTerrassa.push(tempsTerrassa);
+        tempsActualTerrassa = elTempsTerrassa[0].weather[0].main;
+        textElTempsActual.textContent = "elTemps| ".concat(tempsActualTerrassa);
+    });
 }
--- first.ts
+++ first.ts
@@ -8,8 +8,11 @@ const request = {
       },
 }
 
+const API_URL_ELTIEMPO = 'https://api.openweathermap.org/data/2.5/weather?lat=41.56667&lon=2.01667&appid=85cfa2c7d7ecfaba7ec9e19c3e057d04';
+
 let actualJoke:string = "";
 const reportAcudits = [];
+let tempsActualTerrassa = "";
 
 function nextJoke() {
     const jokeText = document.getElementById("apiText");
@@ -34,4 +37,19 @@ function jokeNote(note) {
       
     reportAcudits.push(jokesNotes);
     console.log(reportAcudits);
+}
+
+function elTiempo() {
+    const textElTempsActual = document.getElementById('elTemps');
+    const elTempsTerrassa : any = [];
+    fetch(API_URL_ELTIEMPO)
+        .then((response) => response.json())
+        .then((tempsTerrassa) => {
+            elTempsTerrassa.push(tempsTerrassa);
+ 
+            tempsActualTerrassa = elTempsTerrassa[0].weather[0].main;
+            textElTempsActual.textContent = `elTemps| ${tempsActualTerrassa}`;
+              
+    })
+    
 }
