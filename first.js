2ea062fed36c869bc954d55f3d67729f7ed348f8
NoeliaAlvarez <noelia.ag1990@gmail.com>
2022-09-19 12:53:50 +0200

    N2 Exercici 5 2 API's acudits

--- first.js
+++ first.js
@@ -5,19 +5,40 @@ var request = {
         "Accept": "application/json"
     }
 };
+var API_URL_JOKES_CHUCKNORRIS = 'https://api.chucknorris.io/jokes/random';
 var API_URL_ELTIEMPO = 'https://api.openweathermap.org/data/2.5/weather?lat=41.56667&lon=2.01667&appid=85cfa2c7d7ecfaba7ec9e19c3e057d04';
 var actualJoke = "";
 var reportAcudits = [];
 var tempsActualTerrassa = "";
 function nextJoke() {
+    var numRandom = Math.ceil(Math.random() * (100 - 1 + 1) + 1);
     var jokeText = document.getElementById("apiText");
+    if (numRandom % 2 === 0) {
+        var chuckJoke = randomChuckNorrisJoke();
+        jokeText.textContent = "\"".concat(chuckJoke, "\"");
+    }
+    else {
+        var generalJoke = randomJoke();
+        jokeText.textContent = "\"".concat(generalJoke, "\"");
+    }
+}
+function randomJoke() {
     fetch(API_URL, request)
         .then(function (response) { return response.json(); })
         .then(function (jokes) {
         var tpl = jokes;
         actualJoke = tpl.joke;
-        jokeText.textContent = "\"".concat(actualJoke, "\"");
     });
+    return actualJoke;
+}
+function randomChuckNorrisJoke() {
+    fetch(API_URL_JOKES_CHUCKNORRIS)
+        .then(function (response) { return response.json(); })
+        .then(function (joke) {
+        var jcn = joke;
+        actualJoke = jcn.value;
+    });
+    return actualJoke;
 }
 function jokeNote(note) {
     var data = new Date();
--- first.ts
+++ first.ts
@@ -8,6 +8,8 @@ const request = {
       },
 }
 
+const API_URL_JOKES_CHUCKNORRIS = 'https://api.chucknorris.io/jokes/random';
+
 const API_URL_ELTIEMPO = 'https://api.openweathermap.org/data/2.5/weather?lat=41.56667&lon=2.01667&appid=85cfa2c7d7ecfaba7ec9e19c3e057d04';
 
 let actualJoke:string = "";
@@ -15,14 +17,37 @@ const reportAcudits = [];
 let tempsActualTerrassa = "";
 
 function nextJoke() {
+    let numRandom : number = Math. ceil(Math.random() * (100 - 1 + 1) + 1);
     const jokeText = document.getElementById("apiText");
+
+    if(numRandom % 2 === 0) {
+        const chuckJoke:string = randomChuckNorrisJoke();
+        jokeText.textContent = `"${chuckJoke}"`;
+    }
+    else {
+        const generalJoke : string = randomJoke();
+        jokeText.textContent = `"${generalJoke}"`;
+    }
+}
+
+function randomJoke() {
     fetch(API_URL, request)
-        .then((response) => response.json())
-        .then((jokes) => {
-            const tpl= jokes;
-            actualJoke = tpl.joke;
-            jokeText.textContent = `"${actualJoke}"`;
+    .then((response) => response.json())
+    .then((jokes) => {
+        const tpl= jokes;
+        actualJoke = tpl.joke;
     })
+    return actualJoke;
+}
+
+function randomChuckNorrisJoke() {
+    fetch(API_URL_JOKES_CHUCKNORRIS)
+    .then((response) => response.json())
+    .then((joke) => {
+        const jcn= joke;
+        actualJoke = jcn.value;
+    })
+    return actualJoke;
 }
 
 function jokeNote(note) {
